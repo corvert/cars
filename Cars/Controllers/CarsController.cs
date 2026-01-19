3ea271f7a3e75ef7856e558d2615d2e015c477f7
corvert <orvet.priimagi@gmail.com>
2026-01-19 19:51:54 +0200

    add controller, index and indexviewmodel

---
+++ Cars/Controllers/CarsController.cs
@@ -0,0 +1,29 @@
+﻿using Cars.Models.Cars;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly CarsContext _context;
+        public CarsController(CarsContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Cars.Select(x => new CarsIndexViewModel
+            {
+                Id = x.Id,
+                Make = x.Make,
+                Model = x.Model,
+                Color = x.Color,
+                Doors = x.Doors,
+                FuelType = x.FuelType,
+                CreatedAt = x.CreatedAt,
+            });
+            return View(result);
+        }
+    }
+}
---
+++ Cars/Models/Cars/CarsIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Cars.Models.Cars
+{
+    public class CarsIndexViewModel
+    {
+        public Guid Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Doors { get; set; }
+        public string FuelType { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
