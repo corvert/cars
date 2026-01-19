133534e35f61dedab62259949e6735ccffbbc06b
corvert <orvet.priimagi@gmail.com>
2026-01-19 19:50:53 +0200

    add cars.data

---
+++ Cars.Data/CarsContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Data
+{
+    public class CarsContext :DbContext
+    {
+
+        public DbSet<Cars> Cars { get; set; }
+    }
+}
